24edef107bb53f6966dcb75500d145a7797eb84a
LuanVictr <luanvictor2108@gmail.com>
2023-09-13 18:08:39 -0300

    Build: criando docker-compose com servico de banco de dados Feat: Criando entidade e repositorio User

--- src/main/java/com/travelto/travelto/TraveltoApplication.java
+++ src/main/java/com/travelto/travelto/TraveltoApplication.java
@@ -2,12 +2,13 @@ package com.travelto.travelto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.travelto.travelto.model.entities")
 public class TraveltoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TraveltoApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/travelto/travelto/model/entities/User.java
@@ -0,0 +1,100 @@
+package com.travelto.travelto.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "User")
+public class User implements UserDetails {
+
+  @Id
+  private Long id;
+
+  private String name;
+
+  private String password;
+
+  private String username;
+
+  private String email;
+
+  private List<String> interests;
+
+  public User() {}
+
+  public User(Long id, String name, String username, String password, String email) {
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Override
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
