3385ceceda20ada8b01befcf33cae1e559542a84
LuanVictr <luanvictor2108@gmail.com>
2023-09-13 16:33:51 -0300

    Build: Starting initial project structure

---
+++ src/main/java/com/travelto/travelto/TraveltoApplication.java
@@ -0,0 +1,13 @@
+package com.travelto.travelto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveltoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveltoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelto/travelto/TraveltoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelto.travelto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveltoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
